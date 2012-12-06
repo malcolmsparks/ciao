87efacf904b2fbb400d896c1eddb190f613c114d
Malcolm Sparks <malcolm@congreve.com>
2012-12-06 03:24:11 +0000

    Filled out project.clj, including a core.logic dependency and a default nrepl server port.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject ciao "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject ciao "0.1.0"
+  :description "Ciao, a core.logic demonstration using Italian grammar rules."
+  :url "http://blog.malcolmsparks.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.logic "0.8.0-beta2"]]
+  :repl-options {:port 6010}
+  )
